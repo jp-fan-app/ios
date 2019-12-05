24be4823e855f5c76f2a1056596a8b7b6f4785a8
Christoph <cp@thepeaklab.com>
2019-12-05 08:38:12 +0100

    Add Car Stage Cache

--- JPPerformance/Cache/Cache+CarStages.swift
+++ JPPerformance/Cache/Cache+CarStages.swift
@@ -6,4 +6,35 @@
 //  Copyright © 2019 Christoph Pageler. All rights reserved.
 //
 
-import Foundation
+
+import Cache
+
+
+public extension Cache {
+
+    private typealias CodableType = JPFanAppClient.CarStage
+    private var indexKey: String { "carstages-index" }
+    private var indexStorage: DiskStorage<[CodableType]> { carStagesStorage }
+    private var singleStorage: DiskStorage<CodableType> { carStageStorage }
+    private func keyFor(codable: CodableType) -> String? {
+        guard let id = codable.id else { return nil}
+        return "\(id)"
+    }
+
+    func store(carStagesIndex: [JPFanAppClient.CarStage]) {
+        store(index: carStagesIndex,
+              indexStorage: indexStorage,
+              indexKey: indexKey,
+              singleStorage: singleStorage,
+              singleKey: keyFor)
+    }
+
+    func cachedCarStagesIndex() -> [JPFanAppClient.CarStage]? {
+        return cachedIndex(in: indexStorage, key: indexKey)
+    }
+
+    func cached(carStageID: Int) -> JPFanAppClient.CarStage? {
+        return cachedSingle(in: singleStorage, key: "\(carStageID)")
+    }
+
+}
